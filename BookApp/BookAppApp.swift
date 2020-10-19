b3437ce1e877c37a8364f2243acdf5e6da7e396f
Paul Wilkinson <paulw@wilko.me>
2020-10-19 20:30:07 +1100

    Initial Commit

---
+++ BookApp/BookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppApp.swift
+//  BookApp
+//
+//  Created by Paul Wilkinson on 19/10/20.
+//
+
+import SwiftUI
+
+@main
+struct BookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
